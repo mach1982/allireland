333f63f14794bcc167eb336a5c3e2ed728328f3e
mach1982@gmail.com <mach1982@gmail.com>
2018-12-02 23:35:35 +0000

    finished the noumber of wins per provance.

--- main.py
+++ main.py
@@ -45,7 +45,60 @@ def no_of_ulster_wins():
 				ulster_wins += allireland.allireland_winners.get(ulster_teams)
 	return ulster_wins
 
-print(no_of_ulster_wins())
+
+def no_of_connacht_wins():
+
+	no_of_titles_in_connacht=0
+
+	allireland_connacht_list=list(allireland.connacht)
+	allireland_winners_list = list(allireland.allireland_winners)
+
+	connacht_wins=0
+	for i  in range(len(allireland_connacht_list)):
+		if allireland_connacht_list[i] in allireland.allireland_winners :
+			connacht_teams =allireland_connacht_list[i]
+			#print(allireland_connacht_list[i])
+			if allireland.allireland_winners[connacht_teams] > 0 :
+				connacht_wins += allireland.allireland_winners.get(connacht_teams)
+	return connacht_wins
+
+
+
+def no_of_leinster_wins():
+
+	no_of_titles_in_leinster=0
+
+	allireland_leinster_list=list(allireland.leinster)
+	allireland_winners_list = list(allireland.allireland_winners)
+
+	leinster_wins=0
+	for i  in range(len(allireland_leinster_list)):
+		if allireland_leinster_list[i] in allireland.allireland_winners :
+			leinster_teams =allireland_leinster_list[i]
+			#print(allireland_leinster_list[i])
+			if allireland.allireland_winners[leinster_teams] > 0 :
+				leinster_wins += allireland.allireland_winners.get(leinster_teams)
+	return leinster_wins
+
+
+
+def no_of_munster_wins():
+
+	no_of_titles_in_munster=0
+
+	allireland_munster_list=list(allireland.munster)
+	allireland_winners_list = list(allireland.allireland_winners)
+
+	munster_wins=0
+	for i  in range(len(allireland_munster_list)):
+		if allireland_munster_list[i] in allireland.allireland_winners :
+			munster_teams =allireland_munster_list[i]
+			#print(allireland_munster_list[i])
+			if allireland.allireland_winners[munster_teams] > 0 :
+				munster_wins += allireland.allireland_winners.get(munster_teams)
+	return munster_wins
+
+print(no_of_munster_wins())
 
 		
 
